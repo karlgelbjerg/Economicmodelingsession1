f8f2d7190fac4346716e0d8de1d1146f2608e73e
Karlgelbjerg <kgelbjerghan.ieu2022@student.ie.edu>
2024-03-15 11:49:24 +0100

    1st session

--- Econmodel1-2.py
+++ Econmodel1-2.py
@@ -1,3 +1,4 @@
+import random
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -8,3 +9,21 @@ a = Point(2,3)
 b = Point(7,9)
 print(f"a=({a.x}, {a.y})")
 print(f"b=({b.x}, {b.y})")
+
+
+points  = []
+for _ in range(5):
+    x = random.randint(-100, 100)
+    y = random.randint(-100, 100)
+    random_point = Point(x, y)
+    points.append(random_point)
+
+# or in a single line like this
+points.append(Point(random.randint(-100, 100),
+                    random.randint(-100, 100)))
+
+for point in points:
+    print(f"p({point.x}, {point.y})")
+
+#try to print the first point
+print(points[0])
